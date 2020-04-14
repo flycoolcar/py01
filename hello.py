08a7ecc52e2f0550ad415afe01d1addbe5faa358
wangjian <416832754@qq.com>
2020-04-14 13:17:51 +0800

    44
    44

--- hello.py
+++ hello.py
@@ -3,14 +3,22 @@ print('hello')
 print('hello')
 print(1111111111111)
 
-class book：
+class book:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+    
+    def say_price(self):
+        print(f'书的价格是:{self.price}')
 
 
-
-class book_my:
-    def __init__(self,name):
-        super().__init__():
+class book_my(book):
+    def __init__(self,name,price,color):
+        book.__init__(self,name,price)
+        self.color = color
         
+
+
+b1= book('abc',20)
+print(b1.name,b1.price)
+b1.say_price()
