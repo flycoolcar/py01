8d4d20ca3831f14647ca4eb2bf4c580cc3b3c669
wangjian <416832754@qq.com>
2020-04-14 13:02:19 +0800

    44

--- hello.py
+++ hello.py
@@ -2,3 +2,15 @@ print('hello')
 print('hello')
 print('hello')
 print(1111111111111)
+
+class book：
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+
+class book_my:
+    def __init__(self,name):
+        super().__init__():
+        
