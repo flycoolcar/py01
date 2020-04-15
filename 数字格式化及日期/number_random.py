72192971b8ee9a689d375ff239a33be01ed65cf0
WangJian <416832754@qq.com>
2020-04-15 21:37:14 +0800

    ww
    11

---
+++ 数字格式化及日期/number_random.py
@@ -0,0 +1,14 @@
+# f'{3/22:.2%}
+# f'{3/22:.2f}
+# math.trunc(3213213.12313213)
+import random
+import datetime
+
+l = [1, 2, 3, 4, 5, 6, 7, 8]
+random.random()
+random.randint(1, 10)
+random.choice(l)
+random.shuffle(l)
+random.sample(l,2)
+
+# =======datetime======
